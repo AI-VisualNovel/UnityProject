cdd7a7f3ba554e491e2f1620600a0768c5259dd3
zeroan <aaaanching@gmail.com>
2023-07-28 12:16:31 +0800

    change color of slider and slider's topic

---
+++ Assets/Scripts/SettingPage/PrintSlideVolumnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrintSlideVolumnManager : MonoBehaviour
+{
+    private Slider slider;
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VolumeControlManager volumeControlManager = GameObject.FindObjectOfType<VolumeControlManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
