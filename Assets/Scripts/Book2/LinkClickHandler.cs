0ff18f8e870b844f1ad5300f5f5d54d15e91c7c7
kuan0323 <ronni31027@gmail.com>
2023-08-19 17:56:29 +0800

    HelpPage add sound effect when hover link in Book2

--- Assets/Scripts/Book2/LinkClickHandler.cs
+++ Assets/Scripts/Book2/LinkClickHandler.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
 
 public class LinkClickHandler : MonoBehaviour
 {
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        // audioSource = GetComponent<AudioSource>();
+        // audioSource.Stop();
+    }
+
     public void OnLinkClick(string linkID)
     {
         Application.OpenURL(linkID);
@@ -12,5 +21,14 @@ public class LinkClickHandler : MonoBehaviour
     public void OpenGmail()
     {
         Application.OpenURL("mailto:ronni31027@g.ncu.edu.tw");
+    }
+
+    public void OnPointerEnter()
+    {
+        if (audioSource != null)
+        {
+            Debug.Log(999);
+            audioSource.Play();
+        }
     }
 }
