7be89d010079608012d03abe683a239562c2aea3
eleanor128 <eleanor128@gmail.com>
2023-08-30 23:43:21 +0800

    can load previous game and continue playing

--- Assets/Scripts/GamePage/WuxiaStoryController.cs
+++ Assets/Scripts/GamePage/WuxiaStoryController.cs
@@ -14,7 +14,7 @@ namespace OpenAI
     {
         [SerializeField] private InputField inputField;
         [SerializeField] private Button sendButton;
-        [SerializeField] private Text textArea;
+        [SerializeField] private Text textArea;  // 顯示劇情的地方
         [SerializeField] private Button backgroundButton;  
         [SerializeField] private Button textBoxButton;  
         [SerializeField] private ScrollRect scroll;
@@ -86,7 +86,15 @@ namespace OpenAI
 
             if(from_book2 == false){ // 讀檔的過來的話就不用sendreply
                 SendReply(null);
+                // Debug.Log("跑到Start了!!!，from_book2 = false");
+
             }
+            if(from_book2 == true){ // 從book2過來的
+                canMove = true;
+                // Debug.Log("跑到Start了!!!，從book2來");
+                SendPreviousReply(textBoxButton.GetComponentInChildren<Text>().text);
+            }
+            
             lastChangeTime = Time.time;
         }
 
@@ -177,7 +185,7 @@ namespace OpenAI
                 }else{
                     var sentItem = AppendMessage(sentMessage);
                     currentMessageRec = sentItem;
-                    textArea.text = currentMessageRec.GetChild(0).GetChild(0).GetComponent<Text>().text;
+                    textArea.text = currentMessageRec.GetChild(0).GetChild(0).GetComponent<Text>().text; // 這裡!!!
                     suspend = true;
                     textBoxCount = -1;
                 }
@@ -234,9 +242,63 @@ namespace OpenAI
                 }
                 
                 // 存message
-                SaveLoadLegacy.SaveChatMassage(messages); // 這兩行放著不知為啥option就會跳錯
+                SaveLoadLegacy.SaveChatMassage(messages); 
                 SaveLoadLegacy.SaveStoryToList(recMessage.Content);
 
+                
+                GetOptions(recMessage.Content);
+
+                inputField.enabled = true;
+                sendButton.enabled = true;
+
+            }catch(Exception ex){
+                Debug.LogError("An error occurred: " + ex.Message);
+            }
+        }
+
+        private async void SendPreviousReply(string story)
+        {
+           optionChoicing.SetActive(false);
+            try{
+                textBoxCount = 0;
+                imgNeedChange = true;
+                getOptionDone = false;
+
+                
+                
+                var recMessage = new ChatMessage()
+                {
+                    Role = "assistant",
+                    Content = story
+                };
+
+                var recItem = AppendMessage(recMessage);
+
+                
+                inputField.text = "";
+                inputField.enabled = false;
+                sendButton.enabled = false;
+
+                currentMessageRec = recItem;
+                
+
+                scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0);
+                recItem.anchoredPosition = new Vector2(0, -height);
+                LayoutRebuilder.ForceRebuildLayoutImmediate(recItem);
+                height += recItem.sizeDelta.y;
+                scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+                scroll.verticalNormalizedPosition = 0;
+
+                recMessage.Content = recItem.GetChild(0).GetChild(0).GetComponent<Text>().text;
+                messages.Add(recMessage);
+                filteredMessages.Add(recMessage);//此send前的濃縮若慢到這之後才結束會導致刪除到這段記憶，影響嚴重，但基本上不可能那麼慢
+
+
+                chatCount++;
+                if(chatCount >= 2){
+                    messageFilter();
+                    chatCount = 0;
+                }
                 
                 GetOptions(recMessage.Content);
 
