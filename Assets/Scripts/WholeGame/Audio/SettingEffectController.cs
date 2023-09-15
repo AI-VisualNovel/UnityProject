0fc237997363d076debab06e5a0a8c5c89ab8c3f
chieh-eric <ericwang0621@gmail.com>
2023-09-15 16:21:34 +0800

    解決所有音樂撕裂感問題

--- Assets/Scripts/WholeGame/Audio/SettingEffectController.cs
+++ Assets/Scripts/WholeGame/Audio/SettingEffectController.cs
@@ -3,30 +3,52 @@ using System.Collections;
 using UnityEngine.UI;
 public class SettingEffectController : MonoBehaviour
 {
-    [SerializeField] public AudioSource[] BGMSource;
+    [SerializeField] public AudioSource BGMSource;
     [SerializeField] public AudioSource[] SoundEffectSource;
     [SerializeField] public Image targetPanel;
+    [SerializeField] private float bgmMaxVolume = 0.5f;
+    [SerializeField] private int BGSoundLong;
 
 
+    public float fadeInDuration = 2.0f;
+    public float fadeOutDuration = 2.0f;
+
+    private bool isFading = false;
+    private float currentMaxVolume;
+    private void Start()
+    {
+        BGMSource.loop = false;
+
+        StartAudioLoop();
+    }
     void Update()
     {
+        currentMaxVolume = PlayerPrefs.GetFloat("BGMValue");
+
+
+        bgmMaxVolume = currentMaxVolume;
+        BGMSource.volume = bgmMaxVolume;
+
         AdjustVolume();
         AdjustTransparency();
     }
 
     public void AdjustVolume()
     {
-        foreach (AudioSource source in BGMSource)
+        if (!isFading)
         {
-            if (source)
-            {
-                source.volume = PlayerPrefs.GetFloat("BGMValue");
-            }
-            else
-            {
-                Debug.LogWarning("某些BGM可能未正確設定或缺失!");
-            }
+            BGMSource.volume = PlayerPrefs.GetFloat("BGMValue");
         }
+
+        // if (BGMSource)
+        // {
+        //     BGMSource.volume = PlayerPrefs.GetFloat("BGMValue");
+        // }
+        // else
+        // {
+        //     Debug.LogWarning("某些BGM可能未正確設定或缺失!");
+        // }
+
         foreach (AudioSource source in SoundEffectSource)
         {
             if (source)
@@ -46,6 +68,44 @@ public class SettingEffectController : MonoBehaviour
             Color currentColor = targetPanel.color;
             currentColor.a = PlayerPrefs.GetFloat("DTValue");
             targetPanel.color = currentColor;
+        }
+    }
+
+
+
+
+    private void StartAudioLoop()
+    {
+        StartCoroutine(AudioLoop());
+    }
+
+    private IEnumerator AudioLoop()
+    {
+        while (true)
+        {
+            BGMSource.Play();
+
+            isFading = true;
+
+            float fadeInTime = 0;
+            while (fadeInTime < fadeInDuration)
+            {
+                fadeInTime += Time.deltaTime;
+                BGMSource.volume = Mathf.Lerp(0, bgmMaxVolume, fadeInTime / fadeInDuration);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(BGSoundLong - fadeInDuration - fadeOutDuration);
+
+            float fadeOutTime = 0;
+            while (fadeOutTime < fadeOutDuration)
+            {
+                fadeOutTime += Time.deltaTime;
+                BGMSource.volume = Mathf.Lerp(bgmMaxVolume, 0, fadeOutTime / fadeOutDuration);
+                yield return null;
+            }
+
+            isFading = false;
         }
     }
 }
